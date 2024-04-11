cc76c219baff939f17b4929c5c8d471411a72f0a
jason <nftsingularity8@gmail.com>
2024-04-11 11:53:54 +0000

    GPU and CPU utilization

---
+++ PyTorch/work/numpy_to_torch.py
@@ -0,0 +1,49 @@
+import torch
+import numpy as np
+
+"""
+# CONVERTING TENSOR TO NUMPY ARRAY
+# first create a tensor
+a = torch.ones(5)
+print(a)
+
+# initialize a new variable b to torch tensor a
+b = a.numpy()
+print(b)
+print(type(b)) # check type of b
+
+
+# BE CAREFUL WHEN CONVERTING BECASUE IF THE TENSOR OBJECT IS ON GPU AND NOT CPU, AND NUMPY POINTS TO CPU, IT WILL CHANGE THE OBJECT TO CPU.
+# GPU AND CPU ARE MEMORY LOCATIONS
+
+
+a.add_(1)
+print(a)
+print(b) # it will add 1 to each element in the array as well even though we are
+"""
+
+# ===========================================================
+
+# CONVERTING NUMPY ARRAY TO TENSOR
+
+c = np.ones(5)
+print(c)
+
+d = torch.from_numpy(c)
+print(d)
+
+# BE CAREFUL HERE AGAIN, IF YOU MODIFY ONE VARIABLE, IT WILL AFFECT ALL SUBSEQUENT ONES POINTING TO IT
+
+c += 1
+print(c)
+print(d)
+
+# THESE WARNINGS WILL HAPPEN WHEN YOU'RE DEALING WITH GPU, IT WOULD SAY ITS NOT ABLE TO DETECT A GPU OR SOME SIMILAR ERROR, TELLING YOU THE MEMORY YOU'RE POINTING TO DOESNT EXIST.
+# HERES HOW TO TROUBLESHOOT IT
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('GPU Found... Using GPU')
+else:
+    device = torch.device('cpu')
+    print('GPU not found... Using CPU')
